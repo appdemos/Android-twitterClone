5ccbc3594dabccc0125a551113e96e3380493dcd
Juho <juho.lindemark@gmail.com>
2020-01-20 12:58:00 +0200

    Ready

--- app/src/main/java/com/app/mytwitterclone/FeedActivity.java
+++ app/src/main/java/com/app/mytwitterclone/FeedActivity.java
@@ -6,12 +6,15 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import com.parse.FindCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,20 +24,32 @@ public class FeedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
-        ListView listView = findViewById(R.id.feedListView);
-        List<Map<String,String>> tweetData = new ArrayList<>();
+        final ListView listView = findViewById(R.id.feedListView);
+        final List<Map<String,String>> tweetData = new ArrayList<>();
+        setTitle("Feed");
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Tweet");
+        query.whereContainedIn("username", ParseUser.getCurrentUser().getList("isFollowing"));
+        query.orderByDescending("createdAt");
+        query.setLimit(20);
 
-        ParseQuery<ParseObject> query =
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e == null){
+                    for (ParseObject tweet:objects) {
+                        Map<String, String> tweetInfo = new HashMap<>();
+                        tweetInfo.put("content", tweet.getString("tweet"));
+                        tweetInfo.put("username", tweet.getString("username"));
+                        tweetData.add((tweetInfo));
+
+                    }
+                    SimpleAdapter simpleAdapter = new SimpleAdapter(FeedActivity.this, tweetData, android.R.layout.simple_list_item_2, new String[]{"content", "username"}, new int[]{android.R.id.text1,android.R.id.text2});
+                    listView.setAdapter(simpleAdapter);
+                }
+            }
+        });
 
 
-        for (String tweet:
-             ) {
-
-        }
-
-        ParseUser.getCurrentUser().getList("isFollowing");
-
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this, tweetData, android.R.layout.simple_list_item_2, new String[]{"content", "username"}, new int[]{android.R.id.text1,android.R.id.text2});
 
 
 
