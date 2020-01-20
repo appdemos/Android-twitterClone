4487091fcd23134acd16b3339a6e66e43159c48d
Juho <juho.lindemark@gmail.com>
2020-01-20 12:35:51 +0200

    Tweet sending works

--- app/src/main/java/com/app/mytwitterclone/Users.java
+++ app/src/main/java/com/app/mytwitterclone/Users.java
@@ -1,20 +1,31 @@
 package com.app.mytwitterclone;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckedTextView;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.Parse;
 import com.parse.ParseException;
+import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +34,61 @@ public class Users extends AppCompatActivity {
 
     private ArrayList<String> users = new ArrayList<>();
     private ArrayAdapter<String> adapter;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = new MenuInflater(this);
+        menuInflater.inflate(R.menu.twiit_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.tweet){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Send a tweet");
+            final EditText tweetEditText = new EditText(this);
+
+            builder.setView(tweetEditText);
+            builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ParseObject tweet = new ParseObject("Tweet");
+                    tweet.put("tweet", tweetEditText.getText().toString());
+                    tweet.put("username", ParseUser.getCurrentUser().getUsername());
+                    tweet.saveInBackground(new SaveCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                Toast.makeText(Users.this, "Tweet send!", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(Users.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.show();
+
+        }
+        else if(item.getItemId() == R.id.logout){
+            ParseUser.logOut();
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
